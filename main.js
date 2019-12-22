8e7a698df085cb8de96e3456940a44029999f791
Dmitriy.Ett <dmitriy.ett@hotmail.com>
2019-12-22 20:48:10 +0200

    viselica  first 3 tasks

---
+++ main.js
@@ -0,0 +1,60 @@
+//Создаем массив со словами
+let words = [
+    "программа",
+    "макака",
+    "прекрасынй",
+    "оладушек",
+    "мороженное",
+    "колесо",
+    "стручёк"
+];
+// выбираем случайное слово 
+let word = words[Math.floor(Math.random() * words.length)];
+
+//Создаем итоговый массив
+let answerArray = [];
+for (let i = 0; i < word.length; i++) {
+    answerArray[i] = " _ ";
+}
+
+let remainingLetters = word.length;
+let endGame = word.length + 2;
+
+//Итоговый цикл
+while (remainingLetters > 0 && endGame > 0) {
+    // Основной код
+    alert(answerArray.join(" "));
+    
+    let guess = prompt("Угадайте букву или нажмите Отмена для выхода из игры.");
+    if (guess === null) {
+        break;
+    } else if (guess.length !== 1) {
+        alert("Пожалуйста, введите только одну букву.");
+  
+    }else if (guess === " _ "){
+
+        
+    
+    } else {
+        guess = guess.toLowerCase();
+        for (let j = 0; j < word.length; j++)
+            if (word[j] === guess) {
+                answerArray[j] = guess;
+               
+                remainingLetters--;
+            }
+            
+    }
+    endGame--;
+    // Обновляем состояние игры 
+}
+
+
+    // вхождения угаданной буквы 
+if(endGame > 0)   {
+    alert(answerArray.join(" "));
+    alert("Отлично! Было загадано слово " + word);
+
+}else{
+    alert("Извини , но ты проиграл")
+}
